f297c45a590a7ef3d7746cfa817afaf10821e3ef
Dennis Bejze <53354166+Bejdenn@users.noreply.github.com>
2023-06-25 19:12:52 +0200

    Add edge case tests

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/Bejdenn/gensemester
 
-go 1.17
+go 1.20
--- main.go
+++ main.go
@@ -67,7 +67,13 @@ where <config> is a JSON configuration of the semester
 	}
 
 	log.Default().Printf("Starting to generate directories for %s", *s)
-	dirs := generate(s)
+	dirs, err := generate(s)
+	if err != nil {
+		fmt.Printf("gensemester: error while generating: %v", err)
+		os.Exit(1)
+		return
+	}
+
 	for _, d := range dirs {
 		err := os.Mkdir(d, os.ModePerm)
 		if err != nil {
@@ -82,14 +88,37 @@ where <config> is a JSON configuration of the semester
 	log.Default().Println("Finished generating")
 }
 
-func generate(s *Semester) []string {
+func generate(s *Semester) ([]string, error) {
 	dirs := make([]string, 0)
+
+	if isBlank(s.Name) {
+		return []string{}, fmt.Errorf("name must be a non-blank string")
+	}
+	if containsBlank(s.Years) {
+		return []string{}, fmt.Errorf("years must contain at least one non-blank string")
+	}
+	if isBlank(string(s.Type)) {
+		return []string{}, fmt.Errorf("type must be a non-blank string")
+	}
+	if len(s.Courses) == 0 {
+		return []string{}, fmt.Errorf("courses must contain at least one object")
+	}
 
 	root := path.Join(".", fmt.Sprintf("%s_%s_%s", s.Name, strings.Join(s.Years, "_"), s.Type))
 	dirs = append(dirs, root)
 
 	ws := regexp.MustCompile(`\s{1,}`)
-	for _, c := range s.Courses {
+	for i, c := range s.Courses {
+		if isBlank(c.Name) {
+			return []string{}, fmt.Errorf("courses[%d].name must be a non-blank string", i)
+		}
+		if isBlank(c.Abbreviation) {
+			return []string{}, fmt.Errorf("courses[%d].abbreviation must be a non-blank string", i)
+		}
+		if isBlank(c.Teacher) {
+			return []string{}, fmt.Errorf("courses[%d].teacher must be a non-blank string", i)
+		}
+
 		courseDir := path.Join(root, fmt.Sprintf("%s_%s_%s", c.Abbreviation, ws.ReplaceAllString(c.Name, "_"), c.Teacher))
 		dirs = append(dirs, courseDir)
 
@@ -98,5 +127,19 @@ func generate(s *Semester) []string {
 		}
 	}
 
-	return dirs
+	return dirs, nil
+}
+
+func isBlank(s string) bool {
+	return len(strings.TrimSpace(s)) == 0
+}
+
+func containsBlank(list []string) bool {
+	for _, l := range list {
+		if isBlank(l) {
+			return true
+		}
+	}
+
+	return false
 }
